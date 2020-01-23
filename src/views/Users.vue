6bd59f70a5c7abdea749105e3d18ac66e1eb12ee
rudalson <rudalson@gmail.com>
2020-01-23 17:14:53 +0900

    added router children usage #1 router children의 단순 사용 예

---
+++ src/views/Users.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout
+          row wrap pt-5 text-xs-center
+          style="max-width:500px;margin: 0 auto;"
+  >
+    <v-flex xs12>
+      <h1>Users</h1>
+      <p>유저를 검색해 주세요</p>
+    </v-flex>
+    <v-flex xs12>
+      <v-text-field
+              v-model="userId"
+              label="유저 번호를 입력하세요"
+      />
+      <v-btn @click="$router.push({name: 'users-detail', params: {id:userId}})">검색</v-btn>
+    </v-flex>
+    <v-flex xs12>
+      <router-view/>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        userId: null
+      }
+    }
+  }
+</script>
