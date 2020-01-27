39ae91818f45dae69bc85031010af5a2b0158648
rudalson <rudalson@gmail.com>
2020-01-27 17:46:30 +0900

    added the axios login implement
    axios를 통하여 token을 구해오고 token을 가지고 user info를 구해와서 로그인 상태로 만들기

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import router from '../router'
+import axios from "axios"
 
 Vue.use(Vuex)
 
@@ -32,23 +33,35 @@ export default new Vuex.Store({
     },
     actions: {
         // 로그인 시도
-        login({state, commit}, loginObj) {
-            let selectedUser = null
-            state.allUsers.forEach(user => {
-                if (user.email === loginObj.email) {
-                    selectedUser = user
-                }
-            })
-            if (selectedUser === null) {
-                commit('loginError')
-            } else {
-                if (selectedUser.password !== loginObj.password) {
+        login({commit}, loginObj) {
+            axios.post('https://reqres.in/api/login', loginObj)
+                .then(res => {
+                    // 성공시 토큰을 받음(user_id 값도 받아 올것임)
+                    // 토큰을 헤더에 포함시켜서 유저 정보를 요청
+                    let config = {
+                        headers: {
+                            "access-token": res.data.token
+                        }
+                    }
+                    axios
+                        .get('https://reqres.in/api/users/2', config)
+                        .then(response => {
+                            let userInfo = {
+                                id: response.data.data.id,
+                                first_name: response.data.data.first_name,
+                                last_name: response.data.data.last_name,
+                                avatar: response.data.data.avatar
+                            }
+                            commit('loginSuccess', userInfo);
+                            router.push({name: "mypage"});
+                        })
+                        .catch(() => {
+                            alert('이메일과 비밀번호를 확인하세요.')
+                        })
+                })
+                .catch(() => {
                     commit('loginError')
-                } else {
-                    commit('loginSuccess', selectedUser)
-                    router.push({name: "mypage"});
-                }
-            }
+                });
         },
         logout({commit}) {
             commit("logout");
